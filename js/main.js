35ca9e3e04c4e2aad16bdcddd2a7cdb36bb8749e
DESKTOP-PUHCBU0\김영안 <doraaan89@gmail.com>
2021-03-22 21:39:10 +0900

    25까지

--- js/main.js
+++ js/main.js
@@ -127,3 +127,29 @@ promotionToggleBtn.addEventListener('click', function() {
         // .promotion의.클래스목록에.제거
   }
 });
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+  // gsap.to(요소, 지속시간, 옵션{});
+    y: size,
+    // y축: 20
+    repeat: -1,
+    // 반복: 무한대
+    yoyo: true,
+    // 위아래 반복: 사용
+    ease: Power1.easeInOut,
+    // easing
+    delay: random(0, delay)
+    // 지연: 1초이후에 실행
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5 ,15);
+floatingObject('.floating3', 1.5, 20);
